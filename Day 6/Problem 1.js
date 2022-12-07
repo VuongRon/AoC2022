4c365c0ffc12f4ad3c1c6da9078f96fb29b62222
Ronald Vuong <rvuong@sigmasurgical.com>
2022-12-07 00:27:34 -0500

    Day 6

---
+++ Day 6/Problem 1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./problem1input").toString();
+const markerLength = 4;
+let index = 0;
+
+while (index + markerLength - 1 < data.length) {
+  let matchMap = {};
+  let pairChar = "";
+  for (let offset = 0; offset < markerLength; offset++) {
+    const currentIndex = index + offset;
+    if (matchMap.hasOwnProperty(data[currentIndex])) {
+      pairChar = data[currentIndex];
+      matchMap[data[currentIndex]] = matchMap[data[currentIndex]].concat(currentIndex);
+    } else matchMap[data[currentIndex]] = [currentIndex];
+  }
+  if (pairChar === "") break;
+  index = matchMap[pairChar][matchMap[pairChar].length - 2] + 1;
+}
+
+console.log(index + markerLength);
